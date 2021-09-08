4e8ef4c26411e3466690f478001d29010bb2f0a3
Misha Razinkin <razinkin.m.v@gmail.com>
2021-09-08 15:17:43 +0300

    Gitignore created, BankResponse class skeleton added.

---
+++ src/main/java/org/groupOne/BankResponse.java
@@ -0,0 +1,8 @@
+package org.groupOne;
+
+/**
+ * This class represents an abstraction of a bank response.
+ */
+public class BankResponse {
+    //TODO: implement fields like String bankname, BigDecimal course etc.
+}
