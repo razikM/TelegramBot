941dfe7894a34715c42551a64a2d0ba000449305
Misha Razinkin <razinkin.m.v@gmail.com>
2021-09-14 11:12:58 +0300

    Facade implemented.

---
+++ src/main/java/org/groupOne/Facade.java
@@ -0,0 +1,40 @@
+package org.groupOne;
+
+import java.util.List;
+
+public class Facade {
+
+//    private PrivatAPI privatAPI;
+//    private NbuAPI nbuAPI;
+//    private MonoAPI monoAPI;
+//
+//    public Facade(PrivatAPI privatAPI, NbuAPI nbuAPI, MonoAPI monoAPI) {
+//        this.privatAPI = privatAPI;
+//        this.nbuAPI = nbuAPI;
+//        this.monoAPI = monoAPI;
+//    }
+//
+//    public void setPrivatAPI(PrivatAPI privatAPI) {
+//        this.privatAPI = privatAPI;
+//    }
+//
+//    public void setNbuAPI(NbuAPI nbuAPI) {
+//        this.nbuAPI = nbuAPI;
+//    }
+//
+//    public void setMonoAPI(MonoAPI monoAPI) {
+//        this.monoAPI = monoAPI;
+//    }
+//
+//    public static List<BankResponse> getInfo(String bankName){
+//        if(bankName.equals("Privat")){
+//            return privatAPI.getInfo();
+//        } else if (bankName.equals("Nbu")){
+//            return NbuAPI.getInfo();
+//        } else if(bankName.equals("Mono")){
+//            return monoAPI.getInfo();
+//        } else {
+//            throw new RuntimeException("Unknown bank name");
+//        }
+//    }
+}
