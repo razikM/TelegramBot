4032b9331995bb397af49c81f99b5e8c6420a53b
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2021-09-15 17:10:37 +0300

    updated

--- src/main/java/org/groupOne/BankResponse.java
+++
@@ -1,8 +0,0 @@
-package org.groupOne;
-
-/**
- * This class represents an abstraction of a bank response.
- */
-public class BankResponse {
-    //TODO: implement fields like String bankname, BigDecimal course etc.
-}
