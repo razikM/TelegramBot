5f97fff4a72e1e9affa4ec045285306f83b74179
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2021-09-15 21:04:59 +0300

    create

--- src/main/java/org/groupOne/Main.java
+++ src/main/java/org/groupOne/Main.java
@@ -1,20 +1,6 @@
 package org.groupOne;
 
-<<<<<<< HEAD
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new Controller());
-
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-=======
 import org.groupOne.facade.Facade;
 import org.groupOne.nbu_api.NBU_API;
 import org.groupOne.privat_api.PrivatApi;
@@ -22,10 +8,10 @@ import org.groupOne.privat_api.PrivatApi;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+  /*  public static void main(String[] args) throws IOException, InterruptedException {
         Facade facade = new Facade(new PrivatApi(), new NBU_API());
         System.out.println(facade.getInfo("Privat"));
         System.out.println(facade.getInfo("NBU"));
->>>>>>> origin/bank_api
-    }
+
+    }*/
 }
--- src/main/java/org/groupOne/Services/Button.java
+++
@@ -1,7 +0,0 @@
-package org.groupOne.Services;
-
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface Button {
-    void execute(Update update, Settings settings);
-}
