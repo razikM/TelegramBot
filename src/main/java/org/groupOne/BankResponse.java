05264dd5460412e9680c4c30695faea45a6a183e
”Simagingit config --global user.email simagin090@gmail.comgit config --global user.name ”Simagin <simagin090@gmail.com>
2021-09-16 15:51:30 +0300

    MonoBankApi

--- src/main/java/org/groupOne/BankResponse.java
+++ src/main/java/org/groupOne/BankResponse.java
@@ -1,8 +1,64 @@
 package org.groupOne;
 
-/**
- * This class represents an abstraction of a bank response.
- */
+import java.math.BigDecimal;
+
 public class BankResponse {
-    //TODO: implement fields like String bankname, BigDecimal course etc.
+
+    private String bankName;
+    private String currency;
+    private BigDecimal buyRate;
+    private BigDecimal sellRate;
+
+    public BankResponse() {
+
+    }
+
+    public BankResponse(String bankName, String currency, BigDecimal buyRate, BigDecimal sellRate) {
+        this.bankName = bankName;
+        this.currency = currency;
+        this.buyRate = buyRate;
+        this.sellRate = sellRate;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getBuyRate() {
+        return buyRate;
+    }
+
+    public void setBuyRate(BigDecimal buyRate) {
+        this.buyRate = buyRate;
+    }
+
+    public BigDecimal getSellRate() {
+        return sellRate;
+    }
+
+    public void setSellRate(BigDecimal sellRate) {
+        this.sellRate = sellRate;
+    }
+
+    @Override
+    public String toString() {
+        return "BankResponse{" +
+                "bankName='" + bankName + '\'' +
+                ", currency='" + currency + '\'' +
+                ", buyRate=" + buyRate +
+                ", sellRate=" + sellRate +
+                '}';
+    }
 }
